f439bd8d22e4e7a28ea9ecec58a4d19af6456c4a
Vk1817 <pranavreddy1817@gmail.com>
2026-02-03 08:27:59 +0530

    Update Doctors.jsx

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -6,7 +6,7 @@ export default function Doctors() {
       <div className="doctors-grid">
         <div className="doctor-card">
           <img
-            src="https://images.unsplash.com/photo-1606813909355-d65c3b9fd5ab"
+            src="https://thumbs.dreamstime.com/z/happy-male-dentist-clinic-portrait-wearing-lab-coat-sitting-36113336.jpg"
             alt="Dr. Rohit Chougule"
           />
           <h3>Dr. Rohit Chougule</h3>
@@ -15,7 +15,7 @@ export default function Doctors() {
 
         <div className="doctor-card">
           <img
-            src="https://images.unsplash.com/photo-1594824476967-48c8b964273f"
+            src="https://tse2.mm.bing.net/th/id/OIP.Kiegu9ZM9yG8_NaaRDaKqAHaHa?w=626&h=626&rs=1&pid=ImgDetMain&o=7&rm=3"
             alt="Dr. Prangha Chougule"
           />
           <h3>Dr. Prangha Chougule</h3>
