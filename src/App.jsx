3e4cbce999bf6be4c1b9f49e03b4448481ca4ab1
Vk1817 <pranavreddy1817@gmail.com>
2026-02-03 07:37:26 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Services from "./components/Services"
+import Doctors from "./components/Doctors"
+import Contact from "./components/Contact"
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <Doctors />
+      <Contact />
+    </>
+  )
+}
