da4e6dd35d38580658b24c56f3515bd944fa0848
Vk1817 <pranavreddy1817@gmail.com>
2026-02-03 08:13:19 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © {new Date().getFullYear()} Chougule Dental Clinic, Ashta. All Rights Reserved.
+      </p>
+    </footer>
+  )
+}
