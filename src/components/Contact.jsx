b5e69d6a5b3422c828158bd0016a5f2f8ff25109
Vk1817 <pranavreddy1817@gmail.com>
2026-02-06 08:31:12 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ export default function Contact() {
         <div className="contact-map">
           <iframe
             title="Chougule Dental Clinic Location"
-            src="https://maps.app.goo.gl/4bQy6cNTwMo6y9Ar5&output=embed"
+            src="https://www.google.com/maps/place/Chougule+Multispeciality+Dental+Clinic,+Ashta.+Orthodontic+and+Root+canal+Centre/@16.9463331,74.4017317,17z/data=!3m1!4b1!4m6!3m5!1s0x3bc10f374b9a4d81:0xb4788c8d762fca57!8m2!3d16.946328!4d74.4043066!16s%2Fg%2F11nzw3jdk4?entry=ttu&g_ep=EgoyMDI2MDIwMy4wIKXMDSoASAFQAw%3D%3D&output=embed"
             loading="lazy"
           ></iframe>
         </div>
