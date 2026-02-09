fba60ef5252e564858d07fd15edbf94a7a341e2a
Vk1817 <pranavreddy1817@gmail.com>
2026-02-09 14:40:49 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Chougule Dental Clinic</div>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#doctors">Doctors</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
