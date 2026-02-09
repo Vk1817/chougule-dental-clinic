d2df7ec0416a39e8a09fcc3edc466e47cc62c821
Vk1817 <pranavreddy1817@gmail.com>
2026-02-09 14:34:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import FloatingButtons from "./components/FloatingButtons"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Services from "./components/Services"
+import WhyChooseUs from "./components/WhyChooseUs"
 import Doctors from "./components/Doctors"
+import Testimonials from "./components/Testimonials"
 import Contact from "./components/Contact"
+import FloatingButtons from "./components/FloatingButtons"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
-   <>
-  <Hero />
-  <About />
-  <Services />
-  <WhyChooseUs />
-  <Doctors />
-  <Testimonials />
-  <Contact />
-  <FloatingButtons />
-  <Footer />
-</>
-
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <WhyChooseUs />
+      <Doctors />
+      <Testimonials />
+      <Contact />
+      <FloatingButtons />
+      <Footer />
+    </>
   )
 }
